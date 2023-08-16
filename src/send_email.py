3027acc0328b1932339d47644f4eb97c2d071f16
Travis Booth <travswim@gmail.com>
2023-08-15 21:53:11 -0700

    notes for implementation

--- src/send_email.py
+++ src/send_email.py
@@ -1,15 +1,24 @@
 class SendEmail:
     def __init__(self):
+        # TODO: import all configuration, credentials, etc.
+        pass
+
+    def gmail_auth(self):
+        # TODO: create a credential object and authenticate
         pass
 
     def send_email(self):
+        # TODO: self explanatory
         return True
 
-    def read_config(self):
-        pass
-
     def read_template(self):
+        # TODO: read the template file and return the contents
         pass
 
     def format_message(self):
+        # TODO: format the message with the template and data
+        pass
+
+    def reauth(self):
+        # TODO: If the token expires or does not have sufficent permissions, reauthenticate
         pass
