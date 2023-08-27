89868f380304c934a39a7df2eba6c730d502d67e
Travis Booth <travswim@gmail.com>
2023-08-26 22:49:04 -0700

    setup/teadrdown for email tests

--- tests/test_email.py
+++ tests/test_email.py
@@ -3,6 +3,13 @@ from src.send_email import SendEmail
 
 
 class TestSendEmail(unittest.TestCase):
+    def setup(self):
+        # TODO: Generate fake data for testing
+        pass
+
+    def teardown(self):
+        pass
+
     def test_send_email(self):
         email = SendEmail()
-        assert email.send_email() is True
+        assert email.send_email('a', 'b', 'c', 'd') is True
